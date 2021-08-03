e4dee9a15275e58a5b5e4cc43eb4e2479452cf2b
amine feghoul <aminefeghoul55@gmail.com>
2021-08-03 11:35:48 +0100

    strctured folder

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,48 @@
 import './App.css';
-import Navbar from './components/navbar/Navbar';
-
+import {Navbar,Footer} from './components/';
+import {BrowserRouter as Router,Switch,Route} from "react-router-dom"
+import { Home ,About,Contacts,Blog,Login,SignUp} from './pages';
 function App() {
+  const routes = [
+    {
+      route:"/",
+      page: Home
+    },
+    {
+      route:"/about",
+      page:About
+    },
+    {
+      route:"/contacts",
+      page: Contacts
+    },
+    {
+      route:"/blog",
+      page: Blog
+    },
+    {
+      route:"/login",
+      page: Login
+    },
+    {
+      route:"/sign-up",
+      page:SignUp
+    }
+  ]
+  const displayPages = ()=>
+  {
+    return routes.map((route)=> <Route path ={route.route} exact component={route.page} />)
+  }
   return (
     <div className="App">
+        
+        <Router>
         <Navbar/>
+          <Switch>
+            {displayPages()}
+          </Switch>
+          <Footer/>
+        </Router>
     </div>
   );
 }
---
+++ front/src/pages/blog/Blog.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Blog() {
+    return (
+        <div>
+            blog coming soon
+        </div>
+    )
+}
+
+export default Blog
